e92d99c6f1ad1a7917ea3ca478b0344d58762cfe
khoa12444-cmd <khoa12444@gmail.com>
2026-02-23 21:28:28 +0700

    Add files via upload

---
+++ print.py
@@ -0,0 +1,7 @@
+username = input("Nhập username: ")
+password = input("Nhập password: ")
+
+if username == "admin" and password == "123":
+    print("Đăng nhập thành công ✅")
+else:
+    print("Sai tài khoản hoặc mật khẩu ❌")
